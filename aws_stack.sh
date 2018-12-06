93543b4f06dbdf606769c913e4ac2c5274c4e8e8
haggalin <haggalin@gmail.com>
2018-12-06 21:45:59 +0000

    renamed and added some parameter inputs

---
+++ aws_stack.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+programname=$0
+
+function usage {
+    echo "usage: $programname [OPTIONS] [create|update] <STACK_NAME>"
+    echo "  create  creates the stack for the first time on aws"
+    echo "  update  updates an existing stack on aws"
+    echo "  -h      displays this message"
+    exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+    usage
+fi
+
+if [ "$1" -eq "-h"]; then
+    usage
+fi
+
+aws cloudformation ${1}-stack --stack-name ${2} --template-body file://template.yml --capabilities CAPABILITY_IAM
--- launch_stack.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-aws cloudformation create-stack --stack-name $1 --template-body file://template.yml --capabilities CAPABILITY_IAM
